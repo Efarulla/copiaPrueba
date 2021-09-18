9e57dab52d49901c4675d356206fbc9e55a31fa8
steven <efarulla80362@ufide.ac.cr>
2021-09-17 23:49:35 -0600

    esta es otra prueba

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,5 +7,6 @@ namespace Ejercicio1_Faru.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        //esta es otra Prueba
     }
 }
